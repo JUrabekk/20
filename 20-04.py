89edd0353fc6b5a89d67fffe69d155b4122865f1
JUrabekk <allaberganovjurabek07@mail.ru>
2024-01-08 17:28:46 +0500

    Any - what you have done since your last commit

---
+++ 20-04.py
@@ -0,0 +1,10 @@
+#На вход вашей программы передаётся текст файла с программой на языке Python.
+#Ваша задача – посчитать и напечатать число строк кода, содержащих только
+#комментарий (т.е. в которых первый непробельный символ – символ решётки #).
+
+#import sys
+#data = sys.stdin.read()
+#count = list(map(lambda x: x.lstrip().startswith("#"), data.split("\n"))).count(True)
+#print(count)
+
+print("hello world")
